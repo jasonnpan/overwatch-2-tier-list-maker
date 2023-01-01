96a2e9fdea4aa777819c1e763993b872a7f6ba32
Tommy Su <su.tommy23@gmail.com>
2023-01-01 12:51:17 -0500

    initial setup

---
+++ tierlist/src/App.js
@@ -0,0 +1,13 @@
+import TierContainer from "./components/TierContainer/TierContainer"
+import TierListHeader from "./components/TierListHeader/TierListHeader"
+
+function App() {
+  return (
+    <div className="App">
+      <TierListHeader />
+      <TierContainer />
+    </div>
+  );
+}
+
+export default App;
