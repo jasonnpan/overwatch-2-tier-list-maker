688b18d2f20d28ba57a404b849c3c95454c0024b
Tommy Su <su.tommy23@gmail.com>
2023-01-03 14:28:05 -0500

    added dragging between groups

--- tierlist/src/components/TierContainer/TierContainer.jsx
+++ tierlist/src/components/TierContainer/TierContainer.jsx
@@ -1,70 +1,118 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./tierContainer.scss";
 
-function TierContainer() {
+function TierContainer({ data }) {
+    const [list, setList] = useState(data);
+    const [dragging, setDragging] = useState(false);
+
+    const dragItem = useRef();
+    const dragNode = useRef();
+
+    const handleDragStart = (e, params) => {
+        console.log("drag starting..", params);
+        dragItem.current = params;
+        dragNode.current = e.target;
+        dragNode.current.addEventListener("dragend", handleDragEnd);
+        setTimeout(() => {
+            setDragging(true);
+        }, 0);
+    };
+
+    const handleDragEnter = (e, params) => {
+        console.log("Entering drag..", params);
+
+        const currentItem = dragItem.current;
+        if (e.target !== dragNode.current) {
+            console.log("TARGET IS NOT THE SAME");
+            setList((oldList) => {
+                let newList = JSON.parse(JSON.stringify(oldList));
+                newList[params.tierIndex].items.splice(
+                    params.heroIndex,
+                    0,
+                    newList[currentItem.tierIndex].items.splice(
+                        currentItem.heroIndex,
+                        1
+                    )[0]
+                );
+                dragItem.current = params;
+                return newList;
+            });
+        }
+    };
+
+    const handleDragEnd = () => {
+        console.log("Ending drag..");
+        setDragging(false);
+        dragNode.current.removeEventListener("dragend", handleDragEnd);
+        dragItem.current = null;
+        dragNode.current = null;
+    };
+
+    const getStyles = (params) => {
+        const currentItem = dragItem.current;
+        if (
+            currentItem.tierIndex === params.tierIndex &&
+            currentItem.heroIndex === params.heroIndex
+        ) {
+            return "current tierContent";
+        }
+        return "tierContent";
+    };
+
     return (
         <div className="tierContainer">
-            <div className="tiers">
-                <div className="tierHeader">S</div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
-            </div>
-            <div className="tiers">
-                <div className="tierHeader"></div>
-                <div className="tierContentWrapper">
-                    <div className="tierContent"></div>
-                </div>
+            <div className="drag-n-drop">
+                {list.map((tier, tierIndex) => (
+                    <div
+                        key={tier.title}
+                        className="tiers"
+                        onDragEnter={
+                            dragging && !tier.items.length
+                                ? (e) =>
+                                      handleDragEnter(e, {
+                                          tierIndex,
+                                          heroIndex: 0,
+                                      })
+                                : null
+                        }
+                    >
+                        <div className="tierHeader">{tier.title}</div>
+                        <div className="tierContentWrapper">
+                            {tier.items.map((hero, heroIndex) => (
+                                <div
+                                    key={hero.id}
+                                    className={
+                                        dragging
+                                            ? getStyles({
+                                                  tierIndex,
+                                                  heroIndex,
+                                              })
+                                            : "tierContent"
+                                    }
+                                    draggable
+                                    onDragStart={(e) => {
+                                        handleDragStart(e, {
+                                            tierIndex,
+                                            heroIndex,
+                                        });
+                                    }}
+                                    onDragEnter={
+                                        dragging
+                                            ? (e) => {
+                                                  handleDragEnter(e, {
+                                                      tierIndex,
+                                                      heroIndex,
+                                                  });
+                                              }
+                                            : null
+                                    }
+                                >
+                                    {hero}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
